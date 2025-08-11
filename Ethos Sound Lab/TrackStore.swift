f294dc0b92d36fbad0df93967306b84aa7401b1a
3x3x3am <3x3x3am@gmail.com>
2025-08-11 13:44:38 -0700

    Update TrackStore to use StudioTrack and kind parameter

--- Ethos Sound Lab/TrackStore.swift
+++ Ethos Sound Lab/TrackStore.swift
@@ -9,17 +9,17 @@ final class TrackStore: ObservableObject {
 
     func seedDemo() {
         // Mutable demo tracks; adjust later
-        tracks = [
-            TrackModel(name: "Lead Vox"),
-            TrackModel(name: "Backing Vox"),
-            TrackModel(name: "Keys"),
-            TrackModel(name: "Drums")
+           tracks = [
+            TrackModel(name: "Lead Vox", kind: .audio),
+            TrackModel(name: "Backing Vox", kind: .audio),
+            TrackModel(name: "Keys", kind: .audio),
+            TrackModel(name: "Drums", kind: .audio)
         ]
     }
-
     func addTrack(name: String) {
-        tracks.append(TrackModel(name: name))
+        tracks.append(TrackModel(name: name, kind: .audio))
     }
+
 
     func removeTracks(at offsets: IndexSet) {
         tracks.remove(atOffsets: offsets)
